66ea1ebfb4f8e9546fda5a949659d457f01fc97e
jindalabhishek1 <abhishekjindal.jindal74@gmail.com>
2018-08-03 09:22:44 +0530

    practice 30-07-18

---
+++ AbstractClass.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+abstract class Square
+{
+    float side, res;
+    abstract void calculate(float x);
+    void display()
+    {
+        System.out.println("Result:- " + res);
+    }
+}
+
+class Sq extends Square
+{
+    void calculate(float x)
+    {
+        side = x;
+        res = side * side;
+    }
+}
+/*
+class Sqrt extends Square
+{
+    void calculate(int x)
+    {
+        side = x;
+        r =
+    }
+}
+*/
+
+class Cube extends Square
+{
+    void calculate(float x)
+    {
+        side = x;
+        res = side * side * side;
+    }
+}
+
+class AbstractClass
+{
+    public static void main(String[] args)
+    {
+        float x;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        x = sc.nextFloat();
+        Sq obj1 = new Sq();
+        obj1.calculate(x);
+        obj1.display();
+        Cube c = new Cube();
+        c.calculate(x);
+        c.display();
+    }
+}
